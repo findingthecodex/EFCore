871939f27715e96ba4d65643e4e3bd8365b94203
KoreanBadboy <giang_hoang@hotmail.com>
2025-11-20 23:08:01 +0100

    fix stuff

--- obj/Debug/net8.0/EF-Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/EF-Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF-Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c321dc8bdf552a0bea81bbfd9ee8b34153305112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21ebc9ca079f00627a1bc9640c5ac0b2311fe1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF-Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF-Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
